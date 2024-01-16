e65796ca091db90897fedfb89fc8e41efed83279
abhishek-kimbal <abhishek.tyagi@kimbal.io>
2024-01-16 17:39:14 +0530

    Added Real Estate Ads module structure

---
+++ tasks/custom_addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*--
+{
+    "name": "Real Estate Ads",
+    "version": "1.0",
+    "website": "https://theodooguys.com",
+    "author": "Abhishek",
+    "description": """
+    Real estate module to show available properties
+    """,
+    "category": "Sales",
+    "version": '16.0.1.0.0',
+    "depends": [],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+}
